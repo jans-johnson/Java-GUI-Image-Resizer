214e34993c63ccf76d4a5c120216d39b335c906d
Jans Johnson <jaansjohnson@gmail.com>
2021-03-31 07:20:30 +0530

    Cleaned the code

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
 import javax.imageio.ImageWriteParam;
 import javax.imageio.ImageWriter;
-import javax.imageio.stream.ImageOutputStream;
+import javax.imageio.stream.FileImageOutputStream;
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
@@ -17,7 +17,6 @@ import java.nio.file.DirectoryNotEmptyException;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
-import java.util.Iterator;
 import java.util.StringTokenizer;
 
 public class App extends JFrame implements ActionListener
@@ -170,58 +169,55 @@ public class App extends JFrame implements ActionListener
         }
     }
 
-    public static void resize(String inputImagePath,String outputImagePath, int scaledWidth, int scaledHeight) throws IOException
-    {
-        // reads input image
-        File inputFile = new File(inputImagePath);
-        BufferedImage inputImage = ImageIO.read(inputFile);
+    public static void resize(String source, String outputImagePath, int width, int height) throws IOException {
+        File dest=new File(outputImagePath);
+        BufferedImage sourceImage = ImageIO.read(new FileInputStream(source));
+        double ratio = (double) sourceImage.getWidth()/sourceImage.getHeight();
+        if (width < 1) {
+            width = (int) (height * ratio + 0.4);
+        } else if (height < 1) {
+            height = (int) (width /ratio + 0.4);
+        }
 
-        // creates output image
-        BufferedImage outputImage = new BufferedImage(scaledWidth,scaledHeight, inputImage.getType());
-
-        // scales the input image to the output image
-        Graphics2D g2d = outputImage.createGraphics();
-        g2d.drawImage(inputImage, 0, 0, scaledWidth, scaledHeight, null);
-        g2d.dispose();
-
-        // extracts extension of output file
-        String formatName = outputImagePath.substring(outputImagePath.lastIndexOf(".") + 1);
-
-        // writes to output file
-        ImageIO.write(outputImage, formatName, new File(outputImagePath));
+        Image scaled = sourceImage.getScaledInstance(width, height, Image.SCALE_AREA_AVERAGING);
+        BufferedImage bufferedScaled = new BufferedImage(scaled.getWidth(null), scaled.getHeight(null), BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2d = bufferedScaled.createGraphics();
+        g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+        g2d.drawImage(scaled, 0, 0, width, height, null);
+        writeJpeg(bufferedScaled, dest.getCanonicalPath(), 1.0f);
     }
+
+    private static void writeJpeg(BufferedImage image, String destFile, float quality)
+            throws IOException {
+        ImageWriter writer = null;
+        FileImageOutputStream output = null;
+        try {
+            writer = ImageIO.getImageWritersByFormatName("jpeg").next();
+            ImageWriteParam param = writer.getDefaultWriteParam();
+            param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+            param.setCompressionQuality(quality);
+            output = new FileImageOutputStream(new File(destFile));
+            writer.setOutput(output);
+            IIOImage iioImage = new IIOImage(image, null, null);
+            writer.write(null, iioImage, param);
+        } catch (IOException ex) {
+            throw ex;
+        } finally {
+            if (writer != null) {
+                writer.dispose();
+            }
+            if (output != null) {
+                output.close();
+            }
+        }
+    }
+
 
     public static void resize(String inputImagePath,String outputImagePath, float quality) throws IOException
     {
         File imageFile = new File(inputImagePath);
-        File compressedImageFile = new File(outputImagePath);
-
-        // create a BufferedImage as the result of decoding the supplied InputStream
         BufferedImage image = ImageIO.read(new FileInputStream(imageFile));
-
-        // get all image writers for JPG format
-        Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName("jpg");
-
-        if (!writers.hasNext())
-            throw new IllegalStateException("No writers found");
-
-        ImageWriter writer = (ImageWriter) writers.next();
-        ImageOutputStream ios = ImageIO.createImageOutputStream(new FileOutputStream(compressedImageFile));
-        writer.setOutput(ios);
-
-        ImageWriteParam param = writer.getDefaultWriteParam();
-
-        // compress to a given quality
-        param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
-        param.setCompressionQuality(quality);
-
-        // appends a complete image stream containing a single image and
-        //associated stream and image metadata and thumbnails to the output
-        writer.write(null, new IIOImage(image, null, null), param);
-
-        // close all streams
-        ios.close();
-        writer.dispose();
+        writeJpeg(image,outputImagePath,quality);
     }
 
     public static void deleteTemp(String f1,String f2)
@@ -250,119 +246,109 @@ public class App extends JFrame implements ActionListener
     public void actionPerformed(ActionEvent e)
     {
         String s=e.getActionCommand();
-        if(s.equals("Select Image"))
-        {
-            JFileChooser j=new JFileChooser();
-            j.addChoosableFileFilter(new ImageFilter());
-            j.setAcceptAllFileFilterUsed(false);
-            int r=j.showOpenDialog(null);
-            if (r == JFileChooser.APPROVE_OPTION)
-            {
-                path=j.getSelectedFile().getAbsolutePath();
-                pathTF.setText(path);
+        switch (s) {
+            case "Select Image":
+                JFileChooser j = new JFileChooser();
+                j.addChoosableFileFilter(new ImageFilter());
+                j.setAcceptAllFileFilterUsed(false);
+                int r = j.showOpenDialog(null);
+                if (r == JFileChooser.APPROVE_OPTION) {
+                    path = j.getSelectedFile().getAbsolutePath();
+                    pathTF.setText(path);
+                    try {
+                        File inputFile = new File(path);
+                        BufferedImage img = ImageIO.read(inputFile);
+                        widthval = img.getWidth();
+                        heightval = img.getHeight();
+                        ratio = (float) widthval / heightval;
+                        widthTF.setText(String.valueOf(widthval));
+                        heightTF.setText(String.valueOf(heightval));
+                        currentSizeTF.setText((float) inputFile.length() / 1024 + "  kb");
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+                break;
+            case "Check Size":
                 try {
-                    File inputFile = new File(path);
-                    BufferedImage img= ImageIO.read(inputFile);
-                    widthval= img.getWidth();
-                    heightval= img.getHeight();
-                    ratio=(float)widthval/heightval;
-                    widthTF.setText(String.valueOf(widthval));
-                    heightTF.setText(String.valueOf(heightval));
+                    resize(path, "temp.jpg", Integer.parseInt(widthTF.getText()), Integer.parseInt(heightTF.getText()));
+                    File inputFile = new File("temp.jpg");
                     currentSizeTF.setText((float) inputFile.length() / 1024 + "  kb");
-                } catch (IOException e1) {
+                } catch (NumberFormatException | IOException e1) {
                     e1.printStackTrace();
                 }
-            }
-        }
-        else if(s.equals("Check Size"))
-        {
-            try {
-                resize(path, "temp.jpg", Integer.parseInt(widthTF.getText()), Integer.parseInt(heightTF.getText()));
-                File inputFile = new File("temp.jpg");
-                currentSizeTF.setText((float) inputFile.length() / 1024 + "  kb");
-            } catch (NumberFormatException | IOException e1) {
-                e1.printStackTrace();
-            }
-            if(qualitySlider.getValue()!=100)
-            {
-                flag=1;
-                try {
-                    resize("temp.jpg","temp2.jpg" , (float)qualitySlider.getValue()/100);
-                    File inputFile = new File("temp2.jpg");
-                    currentSizeTF.setText((float) inputFile.length() / 1024 + "  kb");
+                if (qualitySlider.getValue() != 100) {
+                    flag = 1;
+                    try {
+                        resize("temp.jpg", "temp2.jpg", (float) qualitySlider.getValue() / 100);
+                        File inputFile = new File("temp2.jpg");
+                        currentSizeTF.setText((float) inputFile.length() / 1024 + "  kb");
 
-                } catch (IOException e1) {
-                    e1.printStackTrace();
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
                 }
-            }
-        }
-        else if(s.equals("Save"))
-        {
-            if(!pathTF.getText().equals(""))
-            try {
-                StringTokenizer st = new StringTokenizer(path,".");
-                if(flag==1)
-                {
-                    resize(path,st.nextToken()+"_Compressed.jpg" , (float)qualitySlider.getValue()/100);
-                    deleteTemp("temp.jpg", "temp2.jpg");
+                break;
+            case "Save":
+                if (!pathTF.getText().equals(""))
+                    try {
+                        StringTokenizer st = new StringTokenizer(path, ".");
+                        if (flag == 1) {
+                            resize(path, st.nextToken() + "_Compressed.jpg", (float) qualitySlider.getValue() / 100);
+                            deleteTemp("temp.jpg", "temp2.jpg");
+                            JOptionPane.showMessageDialog(this,
+                                    "Saved Image",
+                                    "Saved",
+                                    JOptionPane.INFORMATION_MESSAGE);
+                        } else {
+                            resize(path, st.nextToken() + "_Compressed.jpg", Integer.parseInt(widthTF.getText()), Integer.parseInt(heightTF.getText()));
+                            deleteTemp("temp.jpg", " ");
+                            JOptionPane.showMessageDialog(this,
+                                    "Saved Image",
+                                    "Saved",
+                                    JOptionPane.INFORMATION_MESSAGE);
+                        }
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                if (pathTF.getText().equals("")) {
                     JOptionPane.showMessageDialog(this,
-                            "Saved Image",
-                            "Saved",
-                            JOptionPane.INFORMATION_MESSAGE);
+                            "Select an image Before saving.",
+                            "Select an image",
+                            JOptionPane.WARNING_MESSAGE);
                 }
-                else
-                {
-                    resize(path,st.nextToken()+"_Compressed.jpg", Integer.parseInt(widthTF.getText()), Integer.parseInt(heightTF.getText()));
-                    deleteTemp("temp.jpg", " ");
-                    JOptionPane.showMessageDialog(this,
-                            "Saved Image",
-                            "Saved",
-                            JOptionPane.INFORMATION_MESSAGE);
-                }
-            }
-            catch (IOException e1) {
-                e1.printStackTrace();
-            }
-            if(pathTF.getText().equals(""))
-            {
-                JOptionPane.showMessageDialog(this,
-                        "Select an image Before saving.",
-                        "Select an image",
-                        JOptionPane.WARNING_MESSAGE);
-            }
 
-            flag=0;
-        }
-        else if(s.equals("Add"))
-        {
-            JTextField field1 = new JTextField();
-            JTextField field2 = new JTextField();
-            JTextField id=new JTextField();
-            JPanel panel = new JPanel(new GridLayout(0, 1));
-            panel.add(new JLabel("ID :"));
-            panel.add(id);
-            panel.add(new JLabel("Width :"));
-            panel.add(field1);
-            panel.add(new JLabel("Height :"));
-            panel.add(field2);
-            int result = JOptionPane.showConfirmDialog(null, panel, "Add Option",
-                    JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
-            if (result == JOptionPane.OK_OPTION) {
+                flag = 0;
+                break;
+            case "Add":
+                JTextField field1 = new JTextField();
+                JTextField field2 = new JTextField();
+                JTextField id = new JTextField();
+                JPanel panel = new JPanel(new GridLayout(0, 1));
+                panel.add(new JLabel("ID :"));
+                panel.add(id);
+                panel.add(new JLabel("Width :"));
+                panel.add(field1);
+                panel.add(new JLabel("Height :"));
+                panel.add(field2);
+                int result = JOptionPane.showConfirmDialog(null, panel, "Add Option",
+                        JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+                if (result == JOptionPane.OK_OPTION) {
+                    try {
+                        addToOptions(id.getText(), field1.getText(), field2.getText());
+                    } catch (IOException ioException) {
+                        ioException.printStackTrace();
+                    }
+                }
+                break;
+            case "Edit":
+                ProcessBuilder pb = new ProcessBuilder("Notepad.exe", "settings/options.txt");
                 try {
-                    addToOptions(id.getText(),field1.getText(),field2.getText());
+                    pb.start();
                 } catch (IOException ioException) {
                     ioException.printStackTrace();
                 }
-            }
-        }
-        else if (s.equals("Edit"))
-        {
-            ProcessBuilder pb = new ProcessBuilder("Notepad.exe", "settings/options.txt");
-            try {
-                pb.start();
-            } catch (IOException ioException) {
-                ioException.printStackTrace();
-            }
+                break;
         }
     }
 }
